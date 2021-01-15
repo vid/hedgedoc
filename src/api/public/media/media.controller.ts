ccbf25a901efb7720b68bd78d8820ab374a5b0b6
Philip Molares <philip.molares@udo.edu>
2021-01-15 18:53:09 +0100

    auth: adds token-auth to public api
    adds auth service
    adds auth module
    adds token-auth strategy
    adds token-auth to all public api calls
    
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/api/public/media/media.controller.ts
+++ src/api/public/media/media.controller.ts
@@ -11,9 +11,9 @@ import {
   Headers,
   NotFoundException,
   Param,
-  Post,
+  Post, Request,
   UnauthorizedException,
-  UploadedFile,
+  UploadedFile, UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -25,6 +25,7 @@ import {
 import { ConsoleLoggerService } from '../../../logger/console-logger.service';
 import { MediaService } from '../../../media/media.service';
 import { MulterFile } from '../../../media/multer-file.interface';
+import { TokenAuthGuard } from '../../../auth/token-auth.guard';
 
 @Controller('media')
 export class MediaController {
@@ -35,14 +36,16 @@ export class MediaController {
     this.logger.setContext(MediaController.name);
   }
 
+  @UseGuards(TokenAuthGuard)
   @Post()
   @UseInterceptors(FileInterceptor('file'))
   async uploadMedia(
+    @Request() req,
     @UploadedFile() file: MulterFile,
     @Headers('HedgeDoc-Note') noteId: string,
   ) {
     //TODO: Get user from request
-    const username = 'hardcoded';
+    const username = req.user.username;
     this.logger.debug(
       `Recieved filename '${file.originalname}' for note '${noteId}' from user '${username}'`,
       'uploadImage',
@@ -64,10 +67,11 @@ export class MediaController {
     }
   }
 
+  @UseGuards(TokenAuthGuard)
   @Delete(':filename')
-  async deleteMedia(@Param('filename') filename: string) {
+  async deleteMedia(@Request() req, @Param('filename') filename: string) {
     //TODO: Get user from request
-    const username = 'hardcoded';
+    const username = req.user.username;
     try {
       await this.mediaService.deleteFile(filename, username);
     } catch (e) {
